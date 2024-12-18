53e72b808cca5b1538fdb87c021f0f997977e8f3
sarahwhitehouse17 <sarahwhitehouse81@gmail.com>
2024-12-18 09:02:33 +0100

    Added basic CSS

--- src/Weather.js
+++ src/Weather.js
@@ -2,7 +2,6 @@ import React from "react";
 import "./Weather.css";
 import "bootstrap/dist/css/bootstrap.css";
 
-
 export default function Weather() {
   return (
     <div className="Weather">
@@ -17,24 +16,33 @@ export default function Weather() {
             />
           </div>
           <div className="col-3">
-            <input type="submit" value="search" className="btn btn-primary w-100" />
+            <input
+              type="submit"
+              value="search"
+              className="btn btn-primary w-100"
+            />
           </div>
         </div>
       </form>
       <h1>Results for New York</h1>
-      <div className="row">
+      <div className="row weather-data-spacing">
         <div className="col-6 weather-left-col">
-          <img src="https://ssl.gstatic.com/onebox/weather/64/rain.png" alt="weather-icon" />
-          <p className="temperature">4°C </p>
+          <img
+            src="https://ssl.gstatic.com/onebox/weather/64/rain.png"
+            alt="weather-icon"
+          />
+          <span className="temperature">4 </span>
+          <span className="unit">°C | °F</span>
           <ul>
             <li>Precipiation: 100%</li>
             <li>Humidity: 89%</li>
             <li>Wind: 8km/h</li>
           </ul>
         </div>
-        <div className="col-6">
-          <p className="weather-heading">Weather</p>
+        <div className="col-6 d-flex justify-content-end">
+          <br />
           <ul>
+            <li className="weather-heading">Weather</li>
             <li>Monday 03:00</li>
             <li>Rain</li>
           </ul>
